d79562f7237234f8097ff52174a1a2c17459e480
yanning zhao <zhaoyn96@gmail.com>
2025-08-19 22:44:44 -0300

    fix

--- src/pages/SupplyFormPage.tsx
+++ src/pages/SupplyFormPage.tsx
@@ -106,7 +106,7 @@ const SupplyFormPage = () => {
                       type="text"
                       className="w-full px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                       value={item.unit}
-                      onChange={(e) => handleUpdateSupplyItem(item.unit, 'unit', e.target.value)}
+                      onChange={(e) => handleUpdateSupplyItem(item.id, 'unit', e.target.value)}
                       placeholder="请输入单位名称"
                     />
                   </td>
