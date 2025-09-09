a14479e443c3c05153cb5f43a34d575d842156ae
yanning zhao <zhaoyn96@gmail.com>
2025-09-09 00:51:57 -0300

    add dev server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+const VITE_PROXY_TARGET = 'https://yzkerun.cn';
+
+export default defineConfig(() => ({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: VITE_PROXY_TARGET,
+        changeOrigin: true,
+      },
+    }
+  },
+}));
