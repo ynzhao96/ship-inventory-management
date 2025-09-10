6877004439cdf13bc2f3c976876f2174cb9de0ec
yanning zhao <zhaoyn96@gmail.com>
2025-09-10 00:11:29 -0300

    remove cors settings

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ const allowlist = [
   'http://localhost:5173'
 ];
 
-app.use(cors({
-  origin(origin, cb) {
-    // 允许无 Origin（如服务器到服务器）或在白名单中的 Origin
-    if (!origin || allowlist.includes(origin)) return cb(null, true);
-    return cb(new Error('Not allowed by CORS'));
-  },
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-token', 'x_auth_token'],
-  credentials: true,               // 若需要发送 cookie/凭证
-}));
+// app.use(cors({
+//   origin(origin, cb) {
+//     // 允许无 Origin（如服务器到服务器）或在白名单中的 Origin
+//     if (!origin || allowlist.includes(origin)) return cb(null, true);
+//     return cb(new Error('Not allowed by CORS'));
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'x-token', 'x_auth_token'],
+//   credentials: true,               // 若需要发送 cookie/凭证
+// }));
 
 app.use(express.json());
 
